51da2b02e521b2aa5f1db4d0a4f8339d85b0bf7f
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-08-02 16:26:19 +0200

    mile 2 done

--- js/script.js
+++ js/script.js
@@ -1,37 +1,71 @@
 /*
-0. creo delle variabili
-1. creo i numeri da 1 a 100
-2. controllo i moltiplicatori di 3 e 5 o di entrambi
-3. modifico con una scritta
-4. mostro in console
+. richiamo elementi dal Dom
+. creo delle variabili
+. creo i numeri da 1 a 100
+. controllo i moltiplicatori di 3 e 5 o di entrambi
+. modifico con una scritta
+. mostro in console
+. mostro nel DOM
 */
 
 // FASE PREPARATORIA
+//richiamo elementi dal DOM
+const listContainer = document.getElementById('list');
+
 // creo costanti
 const threeFizz = 'Fizz';
 const fiveBuzz = 'Buzz';
 const fizzBuzz = 'FizzBuzz';
+const list = document.createElement('ul');
 
 // creo variabili
 
 // CREO I NUMERI
 for (let i = 1; i <= 100; i++) {
 
+    //creo un li
+    const listItem = document.createElement('li');
+
     //controllo i resti sia di 5 che di 3
     if (i % 3 === 0 && i % 5 === 0) {
+        //mostro i numeri in console
         console.log(fizzBuzz);
+
+        //monto la lista
+        listItem.append(fizzBuzz);
+        list.appendChild(listItem);
         continue;
     }
     // controllo i resti di 3
     else if (i % 3 === 0) {
+        //mostro i numeri in console
         console.log(threeFizz);
+
+        //monto la lista
+        listItem.append(threeFizz);
+        list.appendChild(listItem);
         continue;
     }
     //controllo i resti di 5
     else if (i % 5 === 0) {
+        //mostro i numeri in console
         console.log(fiveBuzz);
+
+        //monto la lista
+        listItem.append(fiveBuzz);
+        list.appendChild(listItem);
         continue;
     }
-    //mostro i numeri
+    //mostro i numeri in console
     console.log(i);
+
+    //monto la lista
+    listItem.append(i);
+    list.appendChild(listItem);
 }
+
+//mostro nel DOM
+listContainer.appendChild(list);
+
+
+

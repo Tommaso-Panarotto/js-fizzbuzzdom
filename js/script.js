8c6ccb1cd653ad0bd4dbf30b6d7e17930955e505
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-08-02 17:17:47 +0200

    end exercise

--- js/script.js
+++ js/script.js
@@ -20,11 +20,17 @@ const list = document.createElement('ul');
 
 // creo variabili
 
+//Aggiungo classi
+list.className = 'list-mark d-flex flex-wrap gap-4';
+
 // CREO I NUMERI
 for (let i = 1; i <= 100; i++) {
 
     //creo un li
     const listItem = document.createElement('li');
+
+    //aggiungo classi
+    listItem.className = 'square-number d-flex justify-content-center align-items-center fw-semibold';
 
     //controllo i resti sia di 5 che di 3
     if (i % 3 === 0 && i % 5 === 0) {
@@ -33,6 +39,9 @@ for (let i = 1; i <= 100; i++) {
 
         //monto la lista
         listItem.append(fizzBuzz);
+        //aggiugno classe a listitem
+        listItem.classList.add('bg-pink');
+        //lego listItem a list
         list.appendChild(listItem);
         continue;
     }
@@ -43,6 +52,9 @@ for (let i = 1; i <= 100; i++) {
 
         //monto la lista
         listItem.append(threeFizz);
+        //aggiugno classe a listitem
+        listItem.classList.add('bg-green');
+        //lego listItem a list
         list.appendChild(listItem);
         continue;
     }
@@ -53,6 +65,9 @@ for (let i = 1; i <= 100; i++) {
 
         //monto la lista
         listItem.append(fiveBuzz);
+        //aggiugno classe a listitem
+        listItem.classList.add('bg-yellow');
+        //lego listItem a list
         list.appendChild(listItem);
         continue;
     }
@@ -61,6 +76,9 @@ for (let i = 1; i <= 100; i++) {
 
     //monto la lista
     listItem.append(i);
+    //aggiugno classe a listitem
+    listItem.classList.add('bg-blue');
+    //lego listItem a list
     list.appendChild(listItem);
 }
 
